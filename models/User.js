d39bf126fc5fa2bf41ee324383ba614da0a70e8a
wldldlsp <wldldlsp@naver.com>
2021-11-18 15:52:00 +0900

    logout

--- models/User.js
+++ models/User.js
@@ -3,36 +3,35 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10
 const jwt = require('jsonwebtoken')
 
-const userSchema =  mongoose.Schema({
-    name : {
-        type : String,
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
         maxlength: 50
     },
-    email:{
-        type : String,
-        trim:true,
-        unique:1
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
     },
-    password:{
-        type:String,
-        minlength:5
+    password: {
+        type: String,
+        minlength: 5
     },
-    lastname:{
-        type:String,
-        maxlength:50
+    lastname: {
+        type: String,
+        maxlength: 50
     },
-    role:{
-        type:Number,
-        default:0
+    role: {
+        type: Number,
+        default: 0
     },
-    image:String,
-    token:{
-        type:String
+    image: String,
+    token: {
+        type: String
     },
-    tokenExp:{
-        type:Number
+    tokenExp: {
+        type: Number
     }
-
 })
 
 userSchema.pre('save',function( next ){
@@ -81,7 +80,6 @@ userSchema.statics.findByToken = function(token, cb){
     //토큰을 decode 한다.
     jwt.verify(token,'secretToken',function(err,decoded){
         //유저아이디를 이용하여 유저를 찾은 후, 클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰을 비교
-
         user.findOne({ "_id": decoded, "token": token }, function (err, user) {
             if(err) return cb(err);
             cb(null,user)
